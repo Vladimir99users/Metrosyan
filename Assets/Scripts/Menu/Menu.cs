45c588b0ac31fc55bc25d3e2a8c49d146b44201a
Igor Vasilev <Protanker25@gmail.com>
2022-08-17 16:26:21 +1000

    Меню
    Открытие, закрытие

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CanvasGroup))]
+public abstract class Menu : MonoBehaviour
+{
+    public bool IsOpen => _canvasGroup.interactable;
+
+    public UnityEvent Opened;
+    public UnityEvent Closed;
+
+    [SerializeField] private bool _closeOnAwake = true;
+    private CanvasGroup _canvasGroup;
+
+    public virtual void Open()
+    {
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+        _canvasGroup.alpha = 1;
+    }
+    public virtual void Close()
+    {
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+        _canvasGroup.alpha = 0;
+    }
+
+    protected virtual void OnAwake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Awake()
+    {
+        OnAwake();
+
+        if (_closeOnAwake)
+        {
+            Close();
+        }
+    }
+
+}
