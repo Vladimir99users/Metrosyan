ba3fa88d618397ee6f2a067c0daa4376d3e8d6f9
Igor Vasilev <Protanker25@gmail.com>
2022-09-06 19:40:38 +1000

    InputManager - отключение инпута

--- Assets/Player/Scripts/NewPlayerController/InputManadger.cs
+++ Assets/Player/Scripts/NewPlayerController/InputManadger.cs
@@ -1,44 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class InputManadger : MonoBehaviour
+public class InputManadger : MonoBehaviour, IInputLisener
 {
     [SerializeField] private GameObject  _controlCreature;
     private Movement _movement => _controlCreature.GetComponent<Movement>();
     private DirectionMouse _direction => _controlCreature.GetComponent<DirectionMouse>();
     [SerializeField] private InputActionReference _movementAction;
     [SerializeField] private InputActionReference _lookMouseAction;
-    //[SerializeField] private Input _keyBoardInput;
-    private void Awake()
+
+    public void EnableInput()
     {
-       // _keyBoardInput = new Input();
-        //Cursor.visible = false;
-       // EnableInputActons();
+        _movementAction.action.Enable();
+        _lookMouseAction.action.Enable();
+    }
+
+    public void DisableInput()
+    {
+        _movementAction.action.Disable();
+        _lookMouseAction.action.Disable();
     }
 
     private void OnEnable()
     {
-        Debug.Log("Enable");
-       // _movementAction = _keyBoardInput.Keyboard.Movement;
-        //_lookMouseAction = _keyBoardInput.Keyboard.Cursor;
-        EnableInputActons();
-    }
-
-    private void EnableInputActons()
-    {
-        _movementAction.action.Enable();
-        _lookMouseAction.action.Enable();
-        SubscribingActionToEvent();
-    }
-
-    private void SubscribingActionToEvent()
-    {
-
+        EnableInput();
     }
 
     private void OnDisable()
     {
-        _movementAction.action.Disable();
-        _lookMouseAction.action.Disable();
+        DisableInput();
     }
 
 
