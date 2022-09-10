75431fa69171ff6723ae47820c9f0f2b12277488
Vladimir99users <51012754+Vladimir99users@users.noreply.github.com>
2022-09-10 20:11:27 +1000

    ИСправление логики врага. Пофиксил создание существ

--- Assets/EnemySystem/Scripts/Enemy.cs
+++ Assets/EnemySystem/Scripts/Enemy.cs
@@ -2,10 +2,78 @@ using UnityEngine;
 using UnityEngine.AI;
 public abstract class Enemy : MonoBehaviour
 {
+    [SerializeField][Range(0,50)] protected float _patrolRadius = 25f;
+    [SerializeField] private Color _patrolColor = Color.blue;
+    [SerializeField][Range(0,10)] protected float _rangeAttacked = 3f;
+    [SerializeField] private Color _rangeColor = Color.green;
+    [SerializeField][Range(0,10)] protected int _rangeIdleState = 3;
+     [SerializeField] private Color _rangeIdleColor = Color.yellow;
+    [SerializeField][Range(0,50)] protected int _damageSkeletons = 25;
+    [SerializeField][Range(0,10)] protected float _attackSpeed = 1f;
+    [SerializeField]protected Animator _animator;
+    [SerializeField] protected GameObject _dotPatrol;
     [SerializeField] protected StateCreature _stateEnemy = StateCreature.Idle;
     [SerializeField] protected Transform _pointAroundWhichPatrol;
     [SerializeField] protected NavMeshAgent _agent;
     [SerializeField] protected LayerMask _playerMask;
+
+    protected float _attackCooldown = 0f;
+    protected bool _isFindPlayer = false;
+    protected bool _isStoped = false;
+    protected bool _isAttack = false;
+    protected Player _player = null;
+
     public abstract void Init(Transform position);
-    public abstract void MoveAgent(Transform player);
+    protected virtual void MoveAgent(Transform dot)
+    {
+        _agent.SetDestination(dot.transform.position);
+    }
+
+    protected virtual void LookRotationTarget(Transform target)
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation, Time.deltaTime * _agent.angularSpeed);
+    }
+
+    protected abstract void Update();
+    protected abstract void FixedUpdate();
+    protected virtual void AttackTarget()
+    {
+        if(_attackCooldown <= 0)
+        {
+            _player.GetComponent<ITakeDamage>().TakeDamage( _damageSkeletons);
+            _attackCooldown = 1f / _attackSpeed;
+        }
+    }
+
+    protected virtual void FindAndAttackedPlayer()
+    {
+           Collider[] col = Physics.OverlapSphere(gameObject.transform.position,_patrolRadius,_playerMask);
+           if(col.Length <= 0)   return;
+
+             _player = col[0].GetComponent<Player>();
+             LookRotationTarget(_player.transform);
+            if(_isAttack) 
+            {
+                _agent.isStopped = false;
+                MoveAgent(_player.transform);
+            } 
+            else 
+            {
+                _agent.isStopped = true;
+                AnimatorState(StateCreature.Attacked);
+            }
+    }
+
+    protected virtual void OnDrawGizmosSelected()
+    {
+        Gizmos.color =_patrolColor;
+        Gizmos.DrawWireSphere(gameObject.transform.position,_patrolRadius);
+        Gizmos.color = _rangeColor;
+        Gizmos.DrawWireSphere(gameObject.transform.position,_rangeAttacked);
+        Gizmos.color = _rangeIdleColor;
+        Gizmos.DrawWireSphere(gameObject.transform.position,_rangeIdleState);
+    }
+    protected abstract void AnimatorState(StateCreature state);
 }
--- Assets/EnemySystem/Scripts/Skeleton.cs
+++ Assets/EnemySystem/Scripts/Skeleton.cs
@@ -1,30 +1,16 @@
 using UnityEngine;
 using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
-public class Skeleton : Enemy
+sealed class Skeleton : Enemy
 {
-    [SerializeField][Range(0,50)] private float _patrolRadius = 25f;
-    [SerializeField][Range(0,10)] private float _rangeAttacked = 3f;
-    [SerializeField][Range(0,50)] private int _damageSkeletons = 25;
-    [SerializeField][Range(0,10)] private int _rangeIdleState = 3;
-    [SerializeField][Range(0,10)] private float _attackSpeed = 1f;
-    [SerializeField]private Animator _animator;
-    [SerializeField] private GameObject _dotPatrol;
-
-    private float _attackCooldown = 0f;
-    private bool _isFindPlayer = false;
-    private bool _isStoped = false;
-    private bool _isAttack = false;
-    private Player _player = null;
+   
     public override void Init(Transform position)
     {
         var _pointer = Instantiate(_dotPatrol,position);
         _pointAroundWhichPatrol = _pointer.transform;
         _pointer.transform.parent = null;
     }
-
-
-    private void Update()
+    protected override void Update()
     {
         _isFindPlayer = Physics.CheckSphere(gameObject.transform.position,_patrolRadius,_playerMask);
         _isAttack = !(_player != null && Vector3.Distance(gameObject.transform.position, _player.transform.position) < _rangeAttacked);
@@ -38,34 +24,17 @@ public class Skeleton : Enemy
        } else _isStoped = false;
 
         if(_isFindPlayer == true && _isAttack == true)   AnimatorState(StateCreature.Walking);
-
-    
-     
-
     }
-    private void FixedUpdate()
+    protected override void FixedUpdate()
     {
-        Debug.Log("Player = " + _isFindPlayer + " stoping = " + _isStoped);
+    
         if(_isFindPlayer == true)
         {
-           Collider[] col = Physics.OverlapSphere(gameObject.transform.position,_patrolRadius,_playerMask);
-           if(col.Length <= 0)   return;
-            //AnimatorState(StateCreature.Walking);
-             _player = col[0].GetComponent<Player>();
-             LookRotationTarget(_player.transform);
-            if(_isAttack) 
-            {
-                _agent.isStopped = false;
-                MoveAgent(_player.transform);
-            } 
-            else 
-            {
-                _agent.isStopped = true;
-                AnimatorState(StateCreature.Attacked);
-            }
-
+          
+            FindAndAttackedPlayer();
         } else if(_isFindPlayer == false && _isStoped == false) 
         {      
+            _agent.isStopped = false;
             LookRotationTarget(_pointAroundWhichPatrol);
             AnimatorState(StateCreature.Walking);
             MoveAgent(_pointAroundWhichPatrol);
@@ -73,39 +42,8 @@ public class Skeleton : Enemy
 
     }
 
-    public void LookRotationTarget(Transform target)
-    {
-        Vector3 direction = (target.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation, Time.deltaTime * _agent.angularSpeed);
-    }
-    public override void MoveAgent(Transform dot)
-    {
-        _agent.SetDestination(dot.transform.position);
-    }
-
-    public void AttackTarget()
-    {
-        if(_attackCooldown <= 0)
-        {
-            _player.GetComponent<ITakeDamage>().TakeDamage( _damageSkeletons);
-            Debug.Log("Я нанёс = " + _damageSkeletons);
-            _attackCooldown = 1f / _attackSpeed;
-
-        }
-        
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(gameObject.transform.position,_patrolRadius);
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(gameObject.transform.position,_rangeAttacked);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(gameObject.transform.position,_rangeIdleState);
-    }
-    private void AnimatorState(StateCreature state)
+   
+    protected override void AnimatorState(StateCreature state)
     {
         switch (state)
         {
