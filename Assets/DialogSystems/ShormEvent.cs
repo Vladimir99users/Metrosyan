ad22d2351e060dad2410d70d2eca2ca1f0c96503
Igor Vasilev <Protanker25@gmail.com>
2022-09-15 21:54:05 +1000

    Много чего. Исправил диалоговую систему. Переход на мейн сцену

---
+++ Assets/DialogSystems/ShormEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ShormEvent : MonoBehaviour
+{
+    [SerializeField] private GameObject _screen;
+    [SerializeField] private AudioSource _mainAudio;
+    public void Begin()
+    {
+        StartCoroutine(Script());
+    }
+
+    private IEnumerator Script()
+    {
+        _mainAudio.Stop();
+        GetComponent<AudioSource>().Play();
+        _screen.gameObject.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(2);
+    }
+}
