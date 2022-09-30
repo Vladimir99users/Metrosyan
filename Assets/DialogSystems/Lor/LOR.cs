554185f92da325f75215844149fa8e101767bf8e
Vladimir99users <51012754+Vladimir99users@users.noreply.github.com>
2022-09-30 18:33:23 +1000

    Исправил прикол с сундуками

--- Assets/DialogSystems/Lor/LOR.cs
+++ Assets/DialogSystems/Lor/LOR.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class LOR : MonoBehaviour
 {
     [SerializeField]private LocalizationTextFile<Narration> _narration;
+    private bool _isLorRead = false;
+
     public void StartLor()
     {
         OnFirstNarrationEnter();
     }
-    public void OnFirstNarrationEnter()
+
+    private void OnFirstNarrationEnter()
     {
+        _isLorRead = true;
         LORViewer.OnStartLORViewer?.Invoke(_narration.GetTextFile());
+    }
+
+    public void EndLor()
+    {
+        DestroyChest();
+    }
+
+    private void DestroyChest()
+    {
+        if(_isLorRead == true)
+        {
+            Destroy(gameObject);
+        }
     }
 }
